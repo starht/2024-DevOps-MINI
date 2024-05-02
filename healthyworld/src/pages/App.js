fed82fc1dabcdfa12138254e3be0f51801ccb0d9
01022549653@naver.com <>
2024-05-02 16:48:16 +0900

    음식카드 추가

--- healthyworld/src/pages/App.js
+++ healthyworld/src/pages/App.js
@@ -1,11 +1,13 @@
 import '../css/App.css';
+import '../components/FoodCardLeft';
+import '../css/FoodCardLeft.css';
+import FoodCard from '../components/FoodCardLeft';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        start
-      </header>
+      <FoodCard />
     </div>
   );
 }
