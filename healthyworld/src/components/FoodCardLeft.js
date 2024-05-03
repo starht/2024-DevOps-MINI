8cae1bce04462cf58b16fb564a2ae709d7300478
01022549653@naver.com <>
2024-05-03 12:03:27 +0900

    Revert "fear: 음식 카드 api"
    This reverts commit e80c0e29208e10a324c375e037dba9ce575c1c6d.

--- healthyworld/src/components/FoodCardLeft.js
+++ healthyworld/src/components/FoodCardLeft.js
@@ -5,23 +5,6 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 function FoodCardLeft() {
-  // var mysql = require("mysql");
-
-  // var connection = mysql.createConnection({
-  //   host: process.env.RDS_HOSTNAME,
-  //   user: process.env.RDS_USERNAME,
-  //   password: process.env.RDS_PASSWORD,
-  //   port: process.env.RDS_PORT,
-  // });
-
-  // connection.connect(function (err) {
-  //   if (err) {
-  //     console.error("Database connection failed: " + err.stack);
-  //     return;
-  //   }
-
-  //   console.log("Connected to database.");
-  // });
 
   const [foods, setFoods] = useState([]);
   const [isLoadig, setIsLoading] = useState(true);
@@ -47,7 +30,6 @@ function FoodCardLeft() {
       console.log(error);
     }
   };
-  // connection.end();
   return (
     <div>
       {foods.map((food) => (
