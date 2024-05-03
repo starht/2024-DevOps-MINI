7a0f434d320b90fceb54ba3e58167497687e7c81
01022549653@naver.com <>
2024-05-03 11:55:03 +0900

    commit

--- healthyworld/src/components/FoodCardLeft.js
+++ healthyworld/src/components/FoodCardLeft.js
@@ -5,23 +5,23 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 function FoodCardLeft() {
-  var mysql = require("mysql");
+  // var mysql = require("mysql");
 
-  var connection = mysql.createConnection({
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    port: process.env.RDS_PORT,
-  });
+  // var connection = mysql.createConnection({
+  //   host: process.env.RDS_HOSTNAME,
+  //   user: process.env.RDS_USERNAME,
+  //   password: process.env.RDS_PASSWORD,
+  //   port: process.env.RDS_PORT,
+  // });
 
-  connection.connect(function (err) {
-    if (err) {
-      console.error("Database connection failed: " + err.stack);
-      return;
-    }
+  // connection.connect(function (err) {
+  //   if (err) {
+  //     console.error("Database connection failed: " + err.stack);
+  //     return;
+  //   }
 
-    console.log("Connected to database.");
-  });
+  //   console.log("Connected to database.");
+  // });
 
   const [foods, setFoods] = useState([]);
   const [isLoadig, setIsLoading] = useState(true);
