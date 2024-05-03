fbdeac3114f7061057c18c03191c64acdb8a73ba
01022549653@naver.com <>
2024-05-03 09:13:12 +0900

    cardName 수정

--- healthyworld/src/components/FoodCardLeft.js
+++ healthyworld/src/components/FoodCardLeft.js
@@ -4,13 +4,13 @@ import menu from '../assets/images/menu.png';
 function FoodCard() {
   return (
     <div className="FoodCard">
-      <div class = "FoodImgWrapperLeft">
-        <img src={salmon} alt=''class='FoodImgLeft' />
+      <div className = "FoodImgWrapperLeft">
+        <img src={salmon} alt=''className='FoodImgLeft' />
       </div>
-        <div class="FoodExplanationWrapper">
+        <div className="FoodExplanationWrapper">
           <h3>연어 스테이크</h3>
           <p>280 Kcal</p>
-          <button> <img class='menu' src={menu} alt=''/>레시피 저장</button>
+          <button> <img className='menu' src={menu} alt=''/>레시피 저장</button>
         </div>
     </div>
   );
