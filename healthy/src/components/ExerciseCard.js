e752c55be6dae407560034f191973bf2c8d1d352
starht <star2thb@gmail.com>
2024-05-03 13:55:09 +0900

    Feat: 메인으로

--- healthy/src/components/ExerciseCard.js
+++ healthy/src/components/ExerciseCard.js
@@ -1,58 +1,31 @@
 import React, { useState, useEffect } from "react";
 import "../css/components/ExerciseCard.css";
+import PropTypes from "prop-types";
 import addLogo from "../assets/images/List_Add.png";
 
-function ExerciseCard() {
-  const [exercises, setExercises] = useState([]);
-  const API_KEY = process.env.REACT_APP_EXERCISE_KEY;
-
-  useEffect(() => {
-    getExercises();
-  }, []);
-
-  const getExercises = async () => {
-    try {
-      const response = await fetch(
-        "https://api.odcloud.kr/api/15068730/v1/uddi:734ff9bb-3696-4993-a365-c0201eb0a6cd?perPage=360&serviceKey=Fbnk2N4bW%2B4o%2BsaaEFrzSXB9dL%2FMDEKDY1hzbRxbz3fJYZT4HMjw0P%2FJ3xBqwtZBKkHe0%2FgccmushZniXUBdSA%3D%3D"
-        // "https://api.odcloud.kr/api/15068730/v1/uddi:734ff9bb-3696-4993-a365-c0201eb0a6cd?perPage=360&serviceKey=" +
-        //   API_KEY
-      );
-      if (!response.ok) {
-        throw new Error("Failed to fetch exercises");
-      }
-      const json = await response.json();
-      const filteredExercises = json.data.filter(
-        (exercise) => exercise.운동명 === "복싱"
-      );
-      setExercises(filteredExercises);
-    } catch (error) {
-      console.error("Error fetching exercises:", error);
-    }
-  };
-
+function ExerciseCard({ name, kcal, backgroundImage }) {
   return (
-    <div>
-      {exercises.map((exercise) => (
-        <div className="card-container" key={exercise.운동명}>
-          <div className="card-wrapper">
-            <div className="card-header">
-              <div className="btn-box"></div>
-              <div className="cal-box">
-                {exercise.단위체중당에너지소비량} kcal
-              </div>
-            </div>
-            <div className="name-box">
-              <div className="exercise-name">{exercise.운동명}</div>
-              <div className="go-to">
-                <img className="add-logo" src={addLogo} alt="Add Logo" />
-                운동하러 가기
-              </div>
-            </div>
+    <div className="card-container" style={{backgroundImage: `url(${backgroundImage})`}}>
+      <div className="card-wrapper">
+        <div className="card-header">
+          <div className="btn-box"></div>
+          <div className="cal-box">{kcal} kcal</div>
+        </div>
+        <div className="name-box">
+          <div className="exercise-name">{name}</div>
+          <div className="go-to">
+            <img className="add-logo" src={addLogo} alt="Add Logo" />
+            운동하러 가기
           </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
+
+ExerciseCard.propTypes = {
+  name:PropTypes.string.isRequired,
+  kcal: PropTypes.string.isRequired,
+};
 
 export default ExerciseCard;
--- healthy/src/pages/Main.js
+++ healthy/src/pages/Main.js
@@ -1,15 +1,47 @@
-import ExerciseCard from '../components/ExerciseCard';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../css/pages/Main.css';
+import ExerciseCard from '../components/ExerciseCard';
 import FoodCardLeft from '../components/FoodCardLeft';
 import FoodCardRight from '../components/FoodCardRight';
 
-
 function Main() {
+  const [exercises, setExercises] = useState([]);
+  const API_KEY = process.env.REACT_APP_EXERCISE_KEY;
+
+  useEffect(() => {
+    getExercises();
+  }, []);
+
+  const getExercises = async () => {
+    try {
+      const response = await fetch(
+        "https://api.odcloud.kr/api/15068730/v1/uddi:734ff9bb-3696-4993-a365-c0201eb0a6cd?perPage=360&serviceKey=Fbnk2N4bW%2B4o%2BsaaEFrzSXB9dL%2FMDEKDY1hzbRxbz3fJYZT4HMjw0P%2FJ3xBqwtZBKkHe0%2FgccmushZniXUBdSA%3D%3D"
+        // "https://api.odcloud.kr/api/15068730/v1/uddi:734ff9bb-3696-4993-a365-c0201eb0a6cd?perPage=360&serviceKey=" +
+        //   API_KEY
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch exercises");
+      }
+      const json = await response.json();
+      const filteredExercises = json.data.filter(
+        (exercise) => exercise.운동명 === "복싱"
+      );
+      setExercises(filteredExercises);
+    } catch (error) {
+      console.error("Error fetching exercises:", error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <ExerciseCard />
+      {exercises.map((exercise) => (
+        <ExerciseCard
+        key={exercise.운동명}
+        kcal={exercise.단위체중당에너지소비량}
+        name={exercise.운동명}
+        />
+      ))}
         <FoodCardLeft />
         <FoodCardRight />
       </header>
