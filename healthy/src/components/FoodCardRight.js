46bcb3ad1a8970eb39f1081ac289c14ea9cc9250
01022549653@naver.com <>
2024-05-03 13:39:05 +0900

    feat. foodcardleft -> main

--- healthy/src/components/FoodCardRight.js
+++ healthy/src/components/FoodCardRight.js
@@ -1,18 +1,51 @@
 import salmon from '../assets/images/SalmonSteak.jpg';
 import menu from '../assets/images/menu.png';
 import '../css/components/FoodCardRight.css';
+import React from "react";
+import { useState, useEffect } from "react";
 
 function FoodCardRight() {
+  const [foods, setFoods] = useState([]);
+  // const [isLoadig, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getFoods();
+  }, []);
+  const getFoods = async () => {
+    try {
+      const response = await fetch(
+        "https://openapi.foodsafetykorea.go.kr/api/" +
+          process.env.REACT_APP_FOOD_KEY +
+          "/COOKRCP01/json/1/1000/RCP_NM=" +
+          "양배추감자전"
+      );
+      if (!response.ok) {
+        throw new Error("failed to fetch");
+      }
+      const json = await response.json();
+      setFoods(json.COOKRCP01.row);
+      // setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="FoodCard">
-      <div className="FoodExplanationWrapper">
-          <h3>연어 스테이크</h3>
-          <p>280 Kcal</p>
-          <button> <img className='menu' src={menu} alt=''/>레시피 저장</button>
+    <div>
+      {foods.map((food) => (
+        <div className="FoodCard">
+          <div className="FoodExplanationWrapper">
+            <h3>{food.RCP_NM}</h3>
+            <p>{food.INFO_ENG} Kcal</p>
+            <button>
+              <img className="menu" src={menu} alt="" />
+              레시피 저장
+            </button>
+          </div>
+          <div className="FoodImgWrapperRight">
+            <img src={food.ATT_FILE_NO_MK} alt="" className="FoodImgRight" />
+          </div>
         </div>
-      <div className = "FoodImgWrapperRight">
-        <img src={salmon} alt=''className='FoodImgRight' />
-      </div>
+      ))}
     </div>
   );
 }
--- healthy/src/pages/Main.js
+++ healthy/src/pages/Main.js
@@ -1,16 +1,51 @@
-import ExerciseCard from '../components/ExerciseCard';
+import ExerciseCard from "../components/ExerciseCard";
 import React from "react";
-import '../css/pages/Main.css';
-import FoodCardLeft from '../components/FoodCardLeft';
-import FoodCardRight from '../components/FoodCardRight';
-
+import "../css/pages/Main.css";
+import FoodCardLeft from "../components/FoodCardLeft";
+import FoodCardRight from "../components/FoodCardRight";
+import { useState, useEffect } from "react";
 
 function Main() {
+  const [foods, setFoods] = useState([]);
+  // const [isLoadig, setIsLoading] = useState(true);
+  let target = "된장국";
+  if (target !== "") {
+    target = "/RCP_NM=" + target;
+  }
+  useEffect(() => {
+    getFoods();
+  }, []);
+  const getFoods = async () => {
+    try {
+      const response = await fetch(
+        "https://openapi.foodsafetykorea.go.kr/api/" +
+          process.env.REACT_APP_FOOD_KEY +
+          "/COOKRCP01/json/1/10" +
+          target
+      );
+      if (!response.ok) {
+        throw new Error("failed to fetch");
+      }
+      const json = await response.json();
+      setFoods(json.COOKRCP01.row);
+      // setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
         <ExerciseCard />
-        <FoodCardLeft />
+        <div className="FoodLeft">
+          {foods.map((food) => (
+            <FoodCardLeft
+              ATT_FILE_NO_MK={food.ATT_FILE_NO_MK}
+              RCP_NM={food.RCP_NM}
+              INFO_ENG={food.INFO_ENG}
+            />
+          ))}
+        </div>
         <FoodCardRight />
       </header>
     </div>
