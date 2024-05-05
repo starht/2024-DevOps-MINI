b83f3ab525b520d63b6664549e4eb2b64ffc64bb
starht <star2thb@gmail.com>
2024-05-05 22:22:28 +0900

    Feat: 네비게이션바 추가

---
+++ healthy/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../css/components/Navbar.css"
+import logo from "../assets/images/logo.png";
+import search from "../assets/images/search.png"
+
+function Navbar() {
+  return (
+      <div className="nav">
+        <img className="nav-logo" src={logo} />
+        <div className="title">HealthyWorld</div>
+        <div className="nav-wrap">
+          <a className="nav-item" href="/">Home</a>
+          <a className="nav-item" href="/foodsearch">음식 조회</a>
+          <a className="nav-item" href="/exercisesearch">운동 조회</a>
+        </div>
+        <div className="nav-search">
+          <form>
+            <div className="search-wrap">
+              <input className="search-input" type="search" placeholder="Search" />
+              <button className="search-btn" type="submit"></button>
+            </div>
+            <div className="toggle-btn r" id="button-1">
+              <input type="checkbox" className="checkbox" />
+              <div className="food"></div>
+              <div className="exercise"></div>
+            </div>
+          </form>
+        </div>
+        <div className="nav-login">
+          <button className="login" href="/mypage">로그인</button>
+        </div>
+      </div>
+  );
+}
+
+export default Navbar;
--- healthy/src/pages/MyPage.js
+++ healthy/src/pages/MyPage.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
 
 function MyPage() {
   return ( <div>
-    
+    <Navbar></Navbar>
   </div> );
 }
 
