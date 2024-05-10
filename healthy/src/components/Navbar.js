b64685d5e28099457e714038587dd5d3065ca69e
starht <star2thb@gmail.com>
2024-05-10 15:55:04 +0900

    Feat: navbar 반응형

--- healthy/src/components/Navbar.js
+++ healthy/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/components/Navbar.css";
 import logo from "../assets/images/logo.png";
+import hamburger from "../assets/images/hamburger.png";
 import search from "../assets/images/search.png";
 import { useAuth } from "../contexts/AuthContext";
 
@@ -30,63 +31,85 @@ function Navbar({ onSearch, loginShow }) {
     navigate("/");
   };
 
+  const toggleActiveClass = () => {
+    const navItems = document.querySelectorAll(".nav-item");
+    const navSearch = document.querySelectorAll(".nav-search");
+    const navLogin = document.querySelectorAll(".nav-login");
+    navItems.forEach((item) => {
+      item.classList.toggle("active");
+    });
+    navSearch.forEach((item) => {
+      item.classList.toggle("active");
+    });
+    navLogin.forEach((item) => {
+      item.classList.toggle("active");
+    });
+  };
+
   return (
     <div className="nav">
-      <img className="nav-logo" src={logo} alt="Logo" />
-      <div className="title">HealthyWorld</div>
+      <div className="logo-title">
+        <img className="nav-logo" src={logo} alt="Logo" />
+        <div className="title">HealthyWorld</div>
+      </div>
+      <div>
+        <a href="#" className="navbar-toggle" onClick={toggleActiveClass}>
+          <img className="hamburger" src={hamburger} />
+        </a>
+      </div>
       <div className="nav-wrap">
         <a className="nav-item" href="/">
-          Home
+          <div className="nav-text">Home</div>
         </a>
         <a className="nav-item" href="/foodsearch">
-          음식 조회
+          <div className="nav-text">음식 조회</div>
         </a>
         <a className="nav-item" href="/exercisesearch">
-          운동 조회
+          <div className="nav-text">운동 조회</div>
         </a>
       </div>
-      <div className="nav-search">
-        <form onSubmit={handleSubmit}>
-          <div className="search-wrap">
-            <input
-              className="search-input"
-              type="search"
-              placeholder="Search"
-              value={searchQuery}
-              onChange={handleInputChange}
-            />
-            <button className="search-btn-wrap" type="submit">
-              <img className="search-btn" src={search} alt="Search" />
+        <div className="nav-search">
+          <form onSubmit={handleSubmit}>
+            <div className="search-wrap">
+              <input
+                className="search-input"
+                type="search"
+                placeholder="Search"
+                value={searchQuery}
+                onChange={handleInputChange}
+              />
+              <button className="search-btn-wrap" type="submit">
+                <img className="search-btn" src={search} alt="Search" />
+              </button>
+            </div>
+            <div className="toggle-btn r" id="button-1">
+              <input
+                type="checkbox"
+                className="checkbox"
+                onChange={handleCheckboxChange}
+              />
+              <div className="food"></div>
+              <div className="exercise"></div>
+            </div>
+          </form>
+        </div>
+        <div className="nav-login">
+          {isLoggedIn ? (
+            <div>
+              <button className="mypage" onClick={() => navigate("/mypage")}>
+                마이페이지
+              </button>
+              <button className="logout" onClick={handleLogoutClick}>
+                로그아웃
+              </button>
+            </div>
+          ) : (
+            <button className="login" href="/mypage" onClick={loginShow}>
+              로그인
             </button>
-          </div>
-          <div className="toggle-btn r" id="button-1">
-            <input
-              type="checkbox"
-              className="checkbox"
-              onChange={handleCheckboxChange}
-            />
-            <div className="food"></div>
-            <div className="exercise"></div>
-          </div>
-        </form>
+          )}
+        </div>
       </div>
-      <div className="nav-login">
-        {isLoggedIn ? (
-          <div>
-            <button className="mypage" onClick={() => navigate("/mypage")}>
-              마이페이지
-            </button>
-            <button className="logout" onClick={handleLogoutClick}>
-              로그아웃
-            </button>
-          </div>
-        ) : (
-          <button className="login" href="/mypage" onClick={loginShow}>
-            로그인
-          </button>
-        )}
-      </div>
-    </div>
   );
 }
 
